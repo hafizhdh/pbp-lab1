c5c38b98dc94aa43c0037111566e370b5b683d34
hafizhdh <hafizadhyaulhaq16@gmail.com>
2022-09-21 21:46:29 +0700

    Membuat form login

--- wishlist/views.py
+++ wishlist/views.py
@@ -45,3 +45,16 @@ def register(request):
     context = {'form':form}
     return render(request, 'register.html', context)
 
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('wishlist:show_wishlist')
+        else:
+            messages.info(request, 'Username atau Password salah!')
+    context = {}
+    return render(request, 'login.html', context)
+
