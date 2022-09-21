bf09a3eeeaee90bb63b89fd8742e2df7949027e3
hafizhdh <hafizadhyaulhaq16@gmail.com>
2022-09-21 21:42:01 +0700

    Membuat form regitrasi

--- wishlist/views.py
+++ wishlist/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from wishlist.models import BarangWishlist
 from django.http import HttpResponse
 from django.core import serializers
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def show_wishlist(request):
@@ -27,3 +31,17 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data  =  BarangWishlist.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Akun telah berhasil dibuat!')
+            return redirect('wishlist:login')
+    
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
